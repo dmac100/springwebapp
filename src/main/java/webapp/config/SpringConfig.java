a75333c77d4520ba7a2b0f0ba4400f0921314f52
dmac <dmac100@users.noreply.github.com>
2014-03-31 22:37:01 +0100

    Moved some configuration to external xml

--- src/main/java/webapp/config/SpringConfig.java
+++ src/main/java/webapp/config/SpringConfig.java
@@ -1,60 +1,23 @@
 package webapp.config;
 
-import java.util.Properties;
-
 import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 
-import org.apache.commons.dbcp.BasicDataSource;
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.orm.hibernate4.HibernateTransactionManager;
-import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.context.annotation.ImportResource;
 
 import webapp.util.DatabaseUtil;
-import webapp.domain.User;
 
 @Configuration
+@ImportResource("classpath:/webapp/config/ApplicationContext.xml")
 @ComponentScan(basePackages={"webapp"})
-@EnableTransactionManagement
 public class SpringConfig {
-	@Bean
-	public DataSource dataSource() {
-		BasicDataSource dataSource = new BasicDataSource();
-		dataSource.setDriverClassName("org.hsqldb.jdbcDriver");
-		dataSource.setUrl("jdbc:hsqldb:mem:testdb;sql.syntax_mys=true");
-		dataSource.setUsername("sa");
-		dataSource.setPassword("");
-		return dataSource;
-	}
-
-	@Bean
-	public LocalSessionFactoryBean sessionFactory() {
-		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-		
-		sessionFactory.setDataSource(dataSource());
-		
-		Properties hibernateProperties = new Properties();
-		hibernateProperties.put("hibernate.dialect", "org.hibernate.dialect.HSQLDialect");
-		
-		sessionFactory.setHibernateProperties(hibernateProperties);
-		
-		sessionFactory.setAnnotatedClasses(User.class);
-		
-		return sessionFactory;
-	}
-
-	@Bean
-	public HibernateTransactionManager transactionManager() {
-		HibernateTransactionManager hibernateTransactionManager = new HibernateTransactionManager();
-		hibernateTransactionManager.setSessionFactory(sessionFactory().getObject());
-		return hibernateTransactionManager;
-	}
+	@Autowired DataSource dataSource;
 	
 	@PostConstruct
 	public void configureDatabase() {
-		DatabaseUtil.initDatabase(dataSource());
+		DatabaseUtil.initDatabase(dataSource);
 	}
 }
